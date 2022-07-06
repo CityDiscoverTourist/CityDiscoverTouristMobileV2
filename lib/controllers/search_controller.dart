09f1f24e5575028b4bb3e5827b4674c6c3a31aa0
BlackAll2225 <tricuong8635.work@gmail.com>
2022-07-06 22:00:49 +0700

    push searchFunction

---
+++ lib/controllers/search_controller.dart
@@ -0,0 +1,55 @@
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:intl/intl.dart';
+import 'package:travel_hour/models/customer.dart';
+import 'package:travel_hour/routes/app_routes.dart';
+import 'package:travel_hour/services/login_service.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+
+import '../models/city.dart';
+import '../models/quest.dart';
+import '../models/quest_type.dart';
+import '../services/city_service.dart';
+import '../services/quest_service.dart';
+import '../services/questtype_service.dart';
+
+class SearchController extends GetxController {
+  var isLoading = true.obs;
+  var questList = List<Quest>.empty().obs;
+  var textSearch="".obs;
+  TextEditingController textFieldCtrl = TextEditingController();
+
+  @override
+  void onInit() async {
+    super.onInit();
+  }
+
+  @override
+  Future<void> onReady() async {
+    super.onReady();
+    ever(textSearch, (_){
+      fetchQuestFeatureData();
+    });
+  }
+
+  @override
+  void onClose() {}
+   void fetchQuestFeatureData() async {
+    try {
+      isLoading(true);
+      var questListApi = await QuestService.fetchQuestFeatureDataV2(textSearch.value);
+      if (questList != null) {
+        print('Co Roi Ne');
+        questList.assignAll(questListApi!);
+      }
+    } finally {
+      isLoading(false);
+    }
+  }
+}
--- lib/services/quest_service.dart
+++ lib/services/quest_service.dart
@@ -68,6 +68,65 @@ class QuestService {
     return listOfBookings_Accept;
     // }
   }
+  static Future<List<Quest>?> fetchQuestFeatureDataV2(String name) async {
+    // WelcomeController homeController = Get.find<WelcomeController>();
+    var response = await http.get(
+        Uri.parse('https://citytourist.azurewebsites.net/api/v1/quests?Name=${name}&language=0'),
+        headers: {
+          "Accept": "application/json",
+          "content-type": "application/json"
+        });
+    // print("API SCHEDULE Status_code: " '${response.statusCode}');
+    // if (response.statusCode == 200) {
+    Map data = jsonDecode(response.body);
+    // Iterable list = dbc;
+    Iterable list = data['data'];
+    // Iterable list = json.decode(jsonString);
+    // Iterable list = [
+    //   {
+    //     "id": 9,
+    //     "title": "Quest 1",
+    //     "description":
+    //         "Quần đảo Phú Quốc nằm trong vịnh Thái Lan, cách TP HCM khoảng 400 km về hướng tây. Nơi đây thu hút du khách trong và ngoài nước bởi các loại hình du lịch đa dạng, với tài nguyên biển, đảo phong phú; hệ sinh thái rừng, biển đa dạng. Vùng biển Phú Quốc có 22 hòn đảo lớn, nhỏ, tổng diện tích khoảng 589,23 km2. Trong đó, đảo Phú Quốc lớn nhất thường được chia thành bắc đảo và nam đảo. Thị trấn Dương Đông nằm ở trung tâm. Hầu hết điểm tham quan, vui chơi hút khách nằm ở nam đảoQuần đảo Phú Quốc nằm trong vịnh Thái Lan, cách TP HCM khoảng 400 km về hướng tây. Nơi đây thu hút du khách trong và ngoài nước bởi các loại hình du lịch đa dạng, với tài nguyên biển, đảo phong phú; hệ sinh thái rừng, biển đa dạng. Vùng biển Phú Quốc có 22 hòn đảo lớn, nhỏ, tổng diện tích khoảng 589,23 km2. Trong đó, đảo Phú Quốc lớn nhất thường được chia thành bắc đảo và nam đảo. Thị trấn Dương Đông nằm ở trung tâm. Hầu hết điểm tham quan, vui chơi hút khách nằm ở nam đảoQuần đảo Phú Quốc nằm trong vịnh Thái Lan, cách TP HCM khoảng 400 km về hướng tây. Nơi đây thu hút du khách trong và ngoài nước bởi các loại hình du lịch đa dạng, với tài nguyên biển, đảo phong phú; hệ sinh thái rừng, biển đa dạng. Vùng biển Phú Quốc có 22 hòn đảo lớn, nhỏ, tổng diện tích khoảng 589,23 km2. Trong đó, đảo Phú Quốc lớn nhất thường được chia thành bắc đảo và nam đảo. Thị trấn Dương Đông nằm ở trung tâm. Hầu hết điểm tham quan, vui chơi hút khách nằm ở nam đảoQuần đảo Phú Quốc nằm trong vịnh Thái Lan, cách TP HCM khoảng 400 km về hướng tây. Nơi đây thu hút du khách trong và ngoài nước bởi các loại hình du lịch đa dạng, với tài nguyên biển, đảo phong phú; hệ sinh thái rừng, biển đa dạng. Vùng biển Phú Quốc có 22 hòn đảo lớn, nhỏ, tổng diện tích khoảng 589,23 km2. Trong đó, đảo Phú Quốc lớn nhất thường được chia thành bắc đảo và nam đảo. Thị trấn Dương Đông nằm ở trung tâm. Hầu hết điểm tham quan, vui chơi hút khách nằm ở nam đảoQuần đảo Phú Quốc nằm trong vịnh Thái Lan, cách TP HCM khoảng 400 km về hướng tây. Nơi đây thu hút du khách trong và ngoài nước bởi các loại hình du lịch đa dạng, với tài nguyên biển, đảo phong phú; hệ sinh thái rừng, biển đa dạng. Vùng biển Phú Quốc có 22 hòn đảo lớn, nhỏ, tổng diện tích khoảng 589,23 km2. Trong đó, đảo Phú Quốc lớn nhất thường được chia thành bắc đảo và nam đảo. Thị trấn Dương Đông nằm ở trung tâm. Hầu hết điểm tham quan, vui chơi hút khách nằm ở nam đảo",
+    //     "price": 100.000,
+    //     "imagePath":
+    //         'https://statics.vntrip.vn/data-v2/data-guide/img_content/1470302452_anh-5.jpg',
+    //     "estimatedTime": "120",
+    //     "estimatedDistance": "20",
+    //     "availableTime": "2022-05-03T14:26:57.88",
+    //     "createdDate": "2022-05-03T14:26:57.88",
+    //     "updatedDate": "2022-05-03T14:26:57.88",
+    //     "status": "ok",
+    //     "questTypeId": 1,
+    //     "questOwnerId": 2,
+    //     "areaId": 3
+    //   },
+    //   {
+    //     "id": 10,
+    //     "title": "Quest 2",
+    //     "description": "Đây là Description quest 2",
+    //     "price": 110.000,
+    //     "imagePath":
+    //         'https://statics.vntrip.vn/data-v2/data-guide/img_content/1470302452_anh-5.jpg',
+    //     "estimatedTime": "100",
+    //     "estimatedDistance": "2",
+    //     "availableTime": "2022-05-03T13:29:19.9",
+    //     "createdDate": "2022-05-03T13:29:19.9",
+    //     "updatedDate": "2022-05-03T13:29:19.9",
+    //     "status": "string",
+    //     "questTypeId": 1,
+    //     "questOwnerId": 2,
+    //     "areaId": 3
+    //   }
+    // ];
+    final bookingsAccept = list.cast<Map<String, dynamic>>();
+    final listOfBookings_Accept = await bookingsAccept.map<Quest>((json) {
+      return Quest.fromJson(json);
+    }).toList();
+    // print('object');
+    return listOfBookings_Accept;
+    // }
+  }
 
   static Future<List<Quest>?> fetchPuQuestFeatureData(
       String customerId, var language) async {
