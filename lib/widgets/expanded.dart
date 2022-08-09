17a8634ba9ff9839917923701682b9e98a8cafc3
BlackAll2225 <tricuong8635.work@gmail.com>
2022-08-09 21:43:40 +0700

    update quest detail

--- lib/widgets/expanded.dart
+++ lib/widgets/expanded.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_html/flutter_html.dart';
 
 class ExpandedWidget extends StatefulWidget {
   final String text;
@@ -31,25 +32,94 @@ class _ExpandedWidgetState extends State<ExpandedWidget> {
   Widget build(BuildContext context) {
     return Container(
         child: secondHalf.length == ""
-            ? Text(
-                widget.text,
-                textAlign: TextAlign.justify,
-                style: TextStyle(
-                  fontFamily: 'RobotoMono',
-                  fontSize: 16,
-                ),
+            ? 
+            Html(
+                data: widget.text,
+                // customRender: {
+                //     'flutter' : (RenderContext context, Widget child, attributes, _){
+                //       return FlutterLogo(
+                //         style: FlutterLogoStyle.horizontal,
+                //         textColor: Colors.blue,
+                //         size: 100.0,
+                //       );
+                //     }
+                // },
+                style: {
+                  'html': Style(backgroundColor: Colors.white12),
+                  'table': Style(backgroundColor: Colors.grey.shade200),
+                  'td': Style(
+                    backgroundColor: Colors.grey.shade400,
+                    padding: EdgeInsets.all(10),
+                  ),
+                  'th': Style(padding: EdgeInsets.all(10), color: Colors.black),
+                  'tr': Style(
+                      backgroundColor: Colors.grey.shade300,
+                      border: Border(
+                          bottom: BorderSide(color: Colors.greenAccent))),
+                },
+                // onLinkTap: (url){
+                //     print('Open the url $url......');
+                // },
+                // onImageTap: (img){
+                //     print('Image $img');
+                // },
+                // onImageError: (exception, stacktrace){
+                //     print(exception);
+                // },
               )
+            // Text(
+            //     widget.text,
+            //     textAlign: TextAlign.justify,
+            //     style: TextStyle(
+            //       fontFamily: 'RobotoMono',
+            //       fontSize: 16,
+            //     ),
+            //   )
             : Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                  flag?  firstHalf:widget.text,
-                    textAlign: TextAlign.justify,
-                    style: TextStyle(
-                      fontFamily: 'RobotoMono',
-                      fontSize: 16,
-                    ),
+                  // Text(
+                  // flag?  firstHalf:widget.text,
+                  //   textAlign: TextAlign.justify,
+                  //   style: TextStyle(
+                  //     fontFamily: 'RobotoMono',
+                  //     fontSize: 16,
+                  //   ),
+                  // ),
+                  Html(
+                data: flag?  firstHalf:widget.text,
+                // customRender: {
+                //     'flutter' : (RenderContext context, Widget child, attributes, _){
+                //       return FlutterLogo(
+                //         style: FlutterLogoStyle.horizontal,
+                //         textColor: Colors.blue,
+                //         size: 100.0,
+                //       );
+                //     }
+                // },
+                style: {
+                  'html': Style(backgroundColor: Colors.white12),
+                  'table': Style(backgroundColor: Colors.grey.shade200),
+                  'td': Style(
+                    backgroundColor: Colors.grey.shade400,
+                    padding: EdgeInsets.all(10),
                   ),
+                  'th': Style(padding: EdgeInsets.all(10), color: Colors.black),
+                  'tr': Style(
+                      backgroundColor: Colors.grey.shade300,
+                      border: Border(
+                          bottom: BorderSide(color: Colors.greenAccent))),
+                },
+                // onLinkTap: (url){
+                //     print('Open the url $url......');
+                // },
+                // onImageTap: (img){
+                //     print('Image $img');
+                // },
+                // onImageError: (exception, stacktrace){
+                //     print(exception);
+                // },
+              ),
                   InkWell(
                     onTap: () {
                     setState(() {
