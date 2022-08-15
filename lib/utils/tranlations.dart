2b83b3aa663bcafbddb041eb083281f4eaee7e8d
BlackAll2225 <tricuong8635.work@gmail.com>
2022-08-15 16:06:57 +0700

    no message

--- lib/utils/tranlations.dart
+++ lib/utils/tranlations.dart
@@ -270,12 +270,9 @@ class Translation extends Translations {
           "Mua quest di nha": "Please buy some quest",
           "Cau hoi chua co goi y": "This question has no suggestion",
           "send a message..": "Send a message...",
-<<<<<<< HEAD
-          "Quest is not ready yet":"Quest is not ready yet"
-=======
+          "Quest is not ready yet":"Quest is not ready yet",
           "show more": "Show more",
           "collapse": "Collapse"
->>>>>>> ed24cc6c7d80312724f85f95f51e4166fe48d4f1
         },
         'vn': {
           "welcome message":
@@ -549,12 +546,9 @@ class Translation extends Translations {
           "Mua quest di nha": "Hãy mua quest đi nhé",
           "Cau hoi chua co goi y": "Câu hỏi này không có gợi ý",
           "send a message..": "Gửi tin nhắn...",
-<<<<<<< HEAD
-          "Quest is not ready yet":"Quest chưa sẵn sàng"
-=======
+          "Quest is not ready yet":"Quest chưa sẵn sàng",
           "show more": "Xem thêm",
           "collapse": "Thu gọn",
->>>>>>> ed24cc6c7d80312724f85f95f51e4166fe48d4f1
         }
       };
 }
